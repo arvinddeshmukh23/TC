af1c6e665f0675bc920f354412ad5e03d2f7705e
arvinddeshmukh23 <31132248+arvinddeshmukh23@users.noreply.github.com>
2022-07-02 21:15:41 +0530

    login code

--- Code/ITK_Snips.cpp
+++ Code/ITK_Snips.cpp
@@ -1,1 +1,18 @@
 //Sample functions
+//login
+int ITK_user_main(int argc, char* argv[])
+{
+	int iFail = ITK_ok;
+
+	char *users_id, *users_password, *users_group;
+	
+	users_id = ITK_ask_cli_argument("-u=");
+	users_password = ITK_ask_cli_argument("-p=");
+	users_group = ITK_ask_cli_argument("-g=");
+	iFail = ITK_init_module(users_id, users_password, users_group);
+
+	printf("Login Status : %d", iFail);
+	TC_write_syslog("Login Status : %d", iFail);
+
+	return iFail;
+}
